6df93647b2c85ee9055cf4416432da1e6028ac4f
MinhTran <tminh400@gmail.com>
2023-08-15 07:28:06 +0700

    asdf

--- App.js
+++ App.js
@@ -1,30 +1,11 @@
 
 import { StyleSheet, StatusBar} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import StudentsScreen  from './StudentsScreen';
-import MainScreen from './MainScreen';
-import LoginScreen from './LoginScreen';
-import { useEffect } from 'react';
-
-const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <MyStack />
-    </NavigationContainer>  
-  );
-}
-function MyStack() {
-  return (
-    <Stack.Navigator 
-    initialRouteName="Main">
-      <Stack.Screen options={{headerShown: false}} name="Students" component={StudentsScreen} />
-      <Stack.Screen name="Main" component={MainScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
-    </Stack.Navigator>
-  );
+    <StudentsScreen/>
+  )
 }
 const styles = StyleSheet.create({
   container: {
--- LoginErrorCode.js
+++
@@ -1,7 +0,0 @@
-const error_code = Object.freeze(new Map([
-    [1, "Login successfully"],
-    [-1, "Invalid password"],
-    [0, "Invalid username"],
-  ]))
-  
-  export default error_code
